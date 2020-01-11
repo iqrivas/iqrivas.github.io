9631eaafe9e0e0a88ade0154b4a54e6dedb9b8fc
Denisse Rivas <iqrivas@gmail.com>
2020-01-11 13:43:50 -0600

    CreateReactApp Structure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  {/* End of App div */}
+      <h1> Hello World! </h1>
+
+    <p>Here are some web apps:</p>
+          <li><a href="https://infinite-sea-32783.herokuapp.com/">
+            {" "}
+            Voting with Ruby on Rails{" "}
+          </a></li>
+          <li><a href="https://radiant-atoll-20128.herokuapp.com/">
+            {" "}
+            Note Taking with React{" "}
+          </a></li>
+  {/* End of App div */}
+    </div>
+  );
+}
+
+export default App;
