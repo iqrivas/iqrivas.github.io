a2e91ab2f8ab07cb2cdbd4c97203a4644e36aa7a
Denisse Rivas <iqrivas@gmail.com>
2020-05-19 12:55:46 -0500

    Updates

---
+++ precache-manifest.54e482bef811185d9f40bc5b19060603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e551893b1ddb83711bff1393937078f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f466753a621efd09479a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f34f8a6894092d7d689",
+    "url": "/static/js/2.1cdf3df0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1cdf3df0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f466753a621efd09479a",
+    "url": "/static/js/main.9abac963.chunk.js"
+  },
+  {
+    "revision": "0193e257eedcaf3020b9",
+    "url": "/static/js/runtime-main.cfcced09.js"
+  }
+]);
